2adb76fccbc35fd47f77d8d42e6e4d6710471c15
jameshedaweng <heda.weng@gmail.com>
2015-09-26 17:39:29 +0200

    style

--- client/components/login-form.jsx
+++ client/components/login-form.jsx
@@ -1,5 +1,19 @@
 LoginForm = React.createClass({
     render() {
-        return <h1>Login</h1>
+        return (
+          <div className="container-content-small">
+            <div className="branding">
+              <div className="branding-icon">
+                <i className="bi_music-repeat-a"></i>
+              </div>
+              <div className="branding-title">
+                MEDI
+              </div>
+              <div className="branding-subtitle">
+                Inicia Sesión
+              </div>
+            </div>
+          </div>
+        )
     }
 })
--- client/components/profile.jsx
+++ client/components/profile.jsx
@@ -2,6 +2,7 @@ Profile = React.createClass({
   render() {
     return (
       <div className="profile-container container-content-small">
+
       </div>
     )
   }
