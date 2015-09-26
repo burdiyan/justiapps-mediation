93756b425446a0c2f394d4284cd2278ca433f647
Alexandr Burdiyan <burdiyan@gmail.com>
2015-09-26 11:50:49 +0200

    Adding routes

--- client/routes.jsx
+++ client/routes.jsx
@@ -3,3 +3,15 @@ FlowRouter.route('/', {
         React.render(<App />, document.getElementById("render-target"))
     }
 })
+
+FlowRouter.route('/calendar', {
+    action(params) {
+        console.log('calendar')
+    }
+})
+
+FlowRouter.route('/mediators', {
+    action(params) {
+        console.log('mediators')
+    }
+})
