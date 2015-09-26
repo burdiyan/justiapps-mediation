1eae2f1a47cb109735f582e9ce651b5c78f74399
Alexandr Burdiyan <burdiyan@gmail.com>
2015-09-26 11:42:07 +0200

    Moving forward with database seeder

--- server/seeder.js
+++ server/seeder.js
@@ -11,36 +11,106 @@ Meteor.startup(function() {
         }
     })
 
-    // Judge
+    // Judges
     Accounts.createUser({
-        username: 'judge',
-        email: 'judge@mediation.com',
+        username: 'judge1',
+        email: 'judge1@mediation.com',
         password: '123456',
         profile: {
             title: 'judge',
+            firstName: 'Sergio',
+            lastName: 'Garcia',
             tribunal: 'Juzgado 1',
             address: {
                 city: 'Madrid',
-                postal_code: '28000',
+                postalCode: '28000',
+                street: 'Calle Bailen, 45'
+            }
+        }
+    })
+    Accounts.createUser({
+        username: 'judge2',
+        email: 'judge2@mediation.com',
+        password: '123456',
+        profile: {
+            title: 'judge',
+            firstName: 'Inmaculada',
+            lastName: 'Fernández',
+            tribunal: 'Juzgado 1',
+            address: {
+                city: 'Madrid',
+                postalCode: '28000',
                 street: 'Calle Bailen, 45'
             }
         }
     })
 
-    // Mediator
+    // Mediators
     Accounts.createUser({
         username: 'mediator1',
         email: 'mediator1@mediation.com',
         password: '123456',
         profile: {
             title: 'mediator',
+            firstName: '',
+            lastName: '',
             association: 'Asociacion 1',
             association_number: 'ASOC-123456',
             specialization: ['Civil', 'Matrimonial', 'Consumidor'],
             address: {
                 city: 'Madrid',
-                postal_code: '28001',
+                postalCode: '28001',
                 street: 'Calle Sagasta, 20'
+            }
+        }
+    })
+    Accounts.createUser({
+        username: 'mediator2',
+        email: 'mediator2@mediation.com',
+        password: '123456',
+        profile: {
+            title: 'mediator',
+            firstName: 'Fulanito',
+            lastName: 'de Tal',
+            association: 'Asociacion 2',
+            association_number: 'ASOC-1414',
+            specialization: ['Civil', 'Matrimonial', 'Consumidor'],
+            address: {
+                city: 'Madrid',
+                postalCode: '28001',
+                street: 'Calle Sagasta, 20'
+            }
+        }
+    })
+
+    // Customers
+    Accounts.createUser({
+        username: 'catalan',
+        email: 'catalan@mediation.com',
+        password: '123456',
+        profile: {
+            title: 'customer',
+            firstName: 'Guillem',
+            lastName: 'Llull',
+            address: {
+                city: 'Madrid',
+                postalCode: '28004',
+                street: 'Calle Mayor, 10'
+            }
+        }
+    })
+    Accounts.createUser({
+        username: 'andaluza',
+        email: 'andaluza@mediation.com',
+        password: '123456',
+        profile: {
+            title: 'customer',
+            firstName: 'Mariana',
+            lastName: 'Garcia Pérez',
+            address: {
+                city: 'Madrid',
+                postalCode: '28000',
+                street: 'Calle Velazquez, 50'
             }
         }
     })
