6474f5198ddab93696af5e943ba917c773e80916
jameshedaweng <heda.weng@gmail.com>
2015-09-26 11:23:30 +0200

    Add fonts

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,69 @@
 App = React.createClass({
-    render() {
-        return (
-            <p>JustiApps</p>
-        )
-    }
+  render() {
+    return (
+      <div className="container-fluid" id="main-container">
+        <div id="panel-left">
+          <div className="panel-header">
+          </div>
+          <div className="menu-item">
+            <div className="menu-icon">
+              <i className="bi_time-calendar-a"></i>
+            </div>
+            <div className="menu-text">
+              Calendario
+            </div>
+            <div className="menu-arrow">
+            </div>
+          </div>
+          <div className="menu-item">
+            <div className="menu-icon">
+            </div>
+            <div className="menu-text">
+              Mediadores
+            </div>
+            <div className="menu-arrow">
+            </div>
+          </div>
+          <div className="menu-item">
+            <div className="menu-icon">
+            </div>
+            <div className="menu-text">
+              Mis Casos
+            </div>
+            <div className="menu-arrow">
+            </div>
+          </div>
+          <div className="menu-item">
+            <div className="menu-icon">
+            </div>
+            <div className="menu-text">
+              Notificaciones
+            </div>
+            <div className="menu-arrow">
+            </div>
+          </div>
+          <div className="menu-item line-bottom">
+            <div className="menu-icon">
+            </div>
+            <div className="menu-text">
+              Acerca de
+            </div>
+            <div className="menu-arrow">
+            </div>
+          </div>
+          <div className="menu-item menu-bottom">
+            <div className="menu-icon">
+            </div>
+            <div className="menu-text">
+              Perfil
+            </div>
+            <div className="menu-arrow">
+            </div>
+          </div>
+        </div>
+        <div id="panel-main">
+        </div>
+      </div>
+    )
+  }
 });
