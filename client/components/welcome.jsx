02dbdb36c56086fc629d50e22e840e931b255526
jameshedaweng <heda.weng@gmail.com>
2015-09-26 18:23:33 +0200

    welcome

---
+++ client/components/welcome.jsx
@@ -0,0 +1,22 @@
+Welcome = React.createClass({
+    render() {
+        return (
+          <div className="container-content-small">
+            <div className="branding">
+              <div className="branding-icon">
+                <i className="bi_music-repeat-a"></i>
+              </div>
+              <div className="branding-title">
+                MEDI
+              </div>
+              <div className="branding-subtitle">
+                Bienvenido
+              </div>
+            </div>
+            <div className="content-small-text">
+              <p>Hola, <strong> José María</strong>.</p>
+            </div>
+          </div>
+        )
+    }
+})
