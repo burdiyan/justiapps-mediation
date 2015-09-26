0e724a77c52ff36e2e237efcd297b0747126b9dc
jameshedaweng <heda.weng@gmail.com>
2015-09-26 15:37:11 +0200

    Notifications

--- client/components/notifications.jsx
+++ client/components/notifications.jsx
@@ -5,49 +5,49 @@ Notifications = React.createClass({
         <div className="notification unread" onClick={this.toggleReadNotification}>
           <div className="notification-mark"></div>
           <div className="notification-text">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.  Aenean massa.
+            El Juzgado de 1ª Instancia nº 2 de Granada ha enviado notificación al mediador Ernesto San Juan para CONCERTAR CITA INFORMATIVA DE MEDIACIÓN, acordada en el Procedimiento de Separación de Mutuo Acuerdo 515/2015 de Carmen Pérez García y Jordi Andreu Prat.
           </div>
         </div>
         <div className="notification unread" onClick={this.toggleReadNotification}>
           <div className="notification-mark"></div>
           <div className="notification-text">
-            Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
+            El Juzgado de 1ª Instancia nº 2 de Granada ha enviado notificación al mediador Ernesto San Juan SOLICITANDO INFORMACIÓN SOBRE LA MARCHA E INCIDENCIAS DEL PROCESO DE MEDIACIÓN. 
           </div>
         </div>
         <div className="notification" onClick={this.toggleReadNotification}>
           <div className="notification-mark"></div>
           <div className="notification-text">
-            Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+            El Juzgado de 1ª Instancia nº 2 de Granada ha enviado notificación al mediador Ernesto San Juan COMUNICANDO FIN DEL PLAZO PARA CONCLUIR LA MEDIACIÓN Y SOLICITANDO INFORMACIÓN SOBRE SU RESULTADO.
           </div>
         </div>
         <div className="notification unread" onClick={this.toggleReadNotification}>
           <div className="notification-mark"></div>
           <div className="notification-text">
-            In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. 
+            El Juzgado de 1ª Instancia nº 2 de Granada ha enviado notificación al mediador Ernesto San Juan comunicando el VISTO BUENO CON EL ACUERDO DE MEDIACIÓN/ SU NO CONFORMIDAD CON EL ACUERDO DE MEDIACIÓN, SOLICITANDO SU REVISIÓN Y NUEVA REDACCIÓN POR EL EQUIPO DE MEDIACIÓN.
           </div>
         </div>
         <div className="notification" onClick={this.toggleReadNotification}>
           <div className="notification-mark"></div>
           <div className="notification-text">
-            Integer tincidunt. Cras dapibus.
+            El Juzgado de 1ª Instancia nº 2 de Granada ha enviado notificación al mediador Santiago Martínez comunicando el VISTO BUENO CON EL ACUERDO DE MEDIACIÓN/ SU NO CONFORMIDAD CON EL ACUERDO DE MEDIACIÓN, SOLICITANDO SU REVISIÓN Y NUEVA REDACCIÓN POR EL EQUIPO DE MEDIACIÓN.
           </div>
         </div>
         <div className="notification" onClick={this.toggleReadNotification}>
           <div className="notification-mark"></div>
           <div className="notification-text">
-            Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. 
+            El Juzgado de 1ª Instancia nº 2 de Granada ha enviado notificación al mediador Ernesto San Juan comunicando el VISTO BUENO CON EL ACUERDO DE MEDIACIÓN/ SU NO CONFORMIDAD CON EL ACUERDO DE MEDIACIÓN, SOLICITANDO SU REVISIÓN Y NUEVA REDACCIÓN POR EL EQUIPO DE MEDIACIÓN.
           </div>
         </div>
         <div className="notification unread" onClick={this.toggleReadNotification}>
           <div className="notification-mark"></div>
           <div className="notification-text">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+            El Juzgado de 1ª Instancia nº 2 de Granada ha enviado notificación al mediador Santiago Martínez para CONCERTAR CITA INFORMATIVA DE MEDIACIÓN, acordada en el Procedimiento de Separación de Mutuo Acuerdo 511/2015 de María de la Villa y José Sanchez García.
           </div>
         </div>
       </div>
     )
   },
   toggleReadNotification(){
-    $.toggleClass('unread');
+    this.toggleClass('unread');
   }
 })  
