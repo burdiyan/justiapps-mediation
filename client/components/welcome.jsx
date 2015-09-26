de06c2b0654210b19fe2f338e285cc87a19dff7d
Alexandr Burdiyan <burdiyan@gmail.com>
2015-09-26 18:31:20 +0200

    Adding welcome message. Change db seeder

--- client/components/welcome.jsx
+++ client/components/welcome.jsx
@@ -1,22 +1,28 @@
 Welcome = React.createClass({
-    render() {
-        return (
-          <div className="container-content-small">
-            <div className="branding">
-              <div className="branding-icon">
-                <i className="bi_music-repeat-a"></i>
-              </div>
-              <div className="branding-title">
-                MEDI
-              </div>
-              <div className="branding-subtitle">
-                Bienvenido
-              </div>
+  mixins: [ReactMeteorData],
+  getMeteorData() {
+    return {
+      currentUser: Meteor.user()
+    }
+  },
+  render() {
+      return (
+        <div className="container-content-small">
+          <div className="branding">
+            <div className="branding-icon">
+              <i className="bi_music-repeat-a"></i>
             </div>
-            <div className="content-small-text">
-              <p>Hola, <strong> José María</strong>.</p>
+            <div className="branding-title">
+              MEDI
+            </div>
+            <div className="branding-subtitle">
+              Bienvenido
             </div>
           </div>
-        )
-    }
+          <div className="content-small-text">
+            <p>Hola, <strong>{this.data.currentUser.profile.firstName}</strong>!</p>
+          </div>
+        </div>
+      )
+  }
 })
