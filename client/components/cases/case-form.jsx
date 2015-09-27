e8f38113b3561c9b32deaed2a0ceaa3b90371c85
Alexandr Burdiyan <burdiyan@gmail.com>
2015-09-27 10:25:36 +0200

    Case form

--- client/components/cases/case-form.jsx
+++ client/components/cases/case-form.jsx
@@ -1,7 +1,21 @@
 CaseForm = React.createClass({
+    mixins: [React.addons.LinkedStateMixin],
+    getInitialState() {
+        return {
+            caseTitle: '', caseNumber: '',
+            mediator: '', judge: '', customers: [],
+            specialization: '', date: new Date(),
+            description: ''
+        }
+    },
+    handleSubmit(event) {
+        event.preventDefault()
+    },
     render() {
         return (
-            <p>add case</p>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" valueLink={this.linkState('caseTitle')} />
+            </form>
         )
     }
 })  
